e6221d98de24c85a8a21161c4b85130c3376ad3f
Admin <Admin@RADCHENKO-LT>
2020-01-18 13:12:58 +0200

    HW_1

---
+++ CalculationProgramm/CalculationProgramm/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculationProgramm
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private bool isButtonEnabled;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, RoutedEventArgs e)
+        {
+            var stringValue = this.Btn1.Content.ToString();
+            if (Int32.TryParse(stringValue, out int number))
+            {
+                this.TextField.Text += number;
+            }
+
+        }
+    }
+}
