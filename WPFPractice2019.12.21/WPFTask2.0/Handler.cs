a6dc44ffb273313d06c252a954972c22b87d9b5a
Admin <Admin@RADCHENKO-LT>
2020-01-31 00:23:30 +0200

    Task 1_3 (WPFTask3), Task 2 (2.0), Task 3 (3.0) are added.

---
+++ WPFPractice2019.12.21/WPFTask2.0/Handler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTask2._0
+{
+    public class Handler
+    {
+        public Student myStudent { get; set; }
+
+        public Handler()
+        {
+            this.myStudent = new Student("Vadik", "Oladik", true);
+        }
+    }
+}
---
+++ WPFPractice2019.12.21/WPFTask3.0/Task.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFTask3._0
+{
+    public class Task
+    {
+        public string TaskName { get; set; }
+
+        public string Description { get; set; }
+
+        public int Priority { get; set; }
+
+        public Task(string taskName, string description, int priority)
+        {
+            this.TaskName = taskName;
+            this.Description = description;
+            this.Priority = priority;
+        }
+    }
+}
