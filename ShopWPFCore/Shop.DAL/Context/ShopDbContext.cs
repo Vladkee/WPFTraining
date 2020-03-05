78a3681d14acb3745f866f19b26259d78e795a52
Admin <Admin@RADCHENKO-LT>
2020-03-05 21:48:11 +0200

    Shop WPF + EF with repository implementation is added.

---
+++ ShopWPFCore/Shop.DAL/Context/ShopDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.DAL.Context
+{
+    public class ShopDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Database=PracticeShopDb;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ ShopWPFCore/Shop.DAL/Repository/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shop.DAL.Repository
+{
+    public interface IRepository <TEntity> : IDisposable where TEntity : class
+    {
+        public IEnumerable<TEntity> Get();
+
+        public TEntity Get(int id);
+
+        public void Delete(int id);
+
+        public void Delete(TEntity entity);
+
+        public void Update(TEntity entity);
+
+        public void Add(TEntity entity);
+
+        public void Save();
+    }
+}
