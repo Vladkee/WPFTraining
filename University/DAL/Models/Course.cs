023b52fcb045b922bdfc9a4c10c03fd3efccf6ac
Admin <Admin@RADCHENKO-LT>
2020-03-13 08:57:13 +0200

    Practise_3 (22/02/2020) UnitOfWork

---
+++ University/DAL/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace University.DAL.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+
+        public virtual Department Department { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name}";
+        }
+    }
+}
